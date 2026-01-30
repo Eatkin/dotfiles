bdc2de616a5f2ccc6bffa47207794f74fd7b2822
Ed <edward.atkin@edwardatkin.co.uk>
2026-01-30 07:23:11 +0000

    feat(bin): Add core PATH setup

---
+++ bash/bashrc.sh
@@ -0,0 +1,8 @@
+# Guard against double load
+[ -n "$DOTFILES_BASH_LOADED" ] && return
+export DOTFILES_BASH_LOADED=1
+
+for f in env aliases functions; do
+  [ -f "$HOME/dotfiles/bash/$f.sh" ] && source "$HOME/dotfiles/bash/$f.sh"
+done
+
