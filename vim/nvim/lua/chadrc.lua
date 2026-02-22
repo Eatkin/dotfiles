34de6017abc495fb83d3d6855912c6f9ce73cacb
Ed <edwardatkin@edwardatkin.co.uk>
2026-02-22 14:15:42 +0000

    feat(nvim): Add searching hidden/gitignore files

--- vim/nvim/lua/chadrc.lua
+++ vim/nvim/lua/chadrc.lua
@@ -1,24 +1,11 @@
--- This file needs to have same structure as nvconfig.lua 
--- https://github.com/NvChad/ui/blob/v3.0/lua/nvconfig.lua
--- Please read that file to know all available options :( 
-
 ---@type ChadrcConfig
 local M = {}
 
+-- Base46: only UI / theme stuff
 M.base46 = {
-	theme = "onedark",
-
-	-- hl_override = {
-	-- 	Comment = { italic = true },
-	-- 	["@comment"] = { italic = true },
-	-- },
+    theme = "onedark",
 }
 
--- M.nvdash = { load_on_startup = true }
--- M.ui = {
---       tabufline = {
---          lazyload = false
---      }
--- }
+pcall(require, "configs.telescope_toggle")
 
 return M
