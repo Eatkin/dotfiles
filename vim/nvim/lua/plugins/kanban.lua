6fc89f7b20451de18c5f67fe790579099802cad5
Ed <edwardatkin@edwardatkin.co.uk>
2026-02-22 17:12:22 +0000

    feat(kanban): Add kanban.nvim plugin + cheatsheet

---
+++ vim/nvim/lua/plugins/kanban.lua
@@ -0,0 +1,17 @@
+return {
+  "arakkkkk/kanban.nvim",
+  -- Optional
+  dependencies = {
+    "nvim-telescope/telescope.nvim",
+  },
+  lazy=false,
+
+  config = function()
+    require("kanban").setup({
+      markdown = {
+        description_folder = "./tasks/", -- Path to save the file corresponding to the task.
+        list_head = "## ",
+      },
+    })
+  end,
+}
