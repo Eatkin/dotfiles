f595c27e50aee2a4c35c028acd5dc644c41aa657
Ed <edwardatkin@edwardatkin.co.uk>
2026-02-27 21:07:59 +0000

    patch(nvim): Update terminal keybind for normal mode + telescope navigation keybinds

--- vim/nvim/lua/config.lua
+++ vim/nvim/lua/config.lua
@@ -30,8 +30,8 @@ opt.hidden = true
 g.mapleader = " "
 
 -- Disable that annoying auto comment on new line
-vim.cmd('autocmd BufEnter * set formatoptions-=cro')
-vim.cmd('autocmd BufEnter * setlocal formatoptions-=cro')
+vim.cmd "autocmd BufEnter * set formatoptions-=cro"
+vim.cmd "autocmd BufEnter * setlocal formatoptions-=cro"
 
 -- Session management integrated with bufferline / scope
 opt.sessionoptions = {
@@ -52,25 +52,67 @@ vim.g.loaded_ruby_provider = 0
 -- vim.g.loaded_python3_provider = 0
 
 -- Lazy vim
-local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+local lazypath = vim.fn.stdpath "data" .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system({
+  vim.fn.system {
     "git",
     "clone",
     "--filter=blob:none",
     "https://github.com/folke/lazy.nvim.git",
     "--branch=stable", -- latest stable release
     lazypath,
-  })
+  }
 end
 vim.opt.rtp:prepend(lazypath)
 
-require("lazy").setup({
-  require("plugins.theme"),
-  require("plugins.navigation"),
-  require("plugins.buffers"),
-  require("plugins.comment"),
-  require("plugins.session"),
-  require("plugins.code"),
-  require("plugins.terminal"),
-})
+require("lazy").setup {
+  require "plugins.theme",
+  require "plugins.navigation",
+  require "plugins.buffers",
+  require "plugins.comment",
+  require "plugins.session",
+  require "plugins.code",
+  require "plugins.terminal",
+  require "plugins.kanban",
+}
+
+-- Set up Kanban controls - creates .kanban.md in root when used
+local kanban_state = {
+  winid = nil,
+}
+
+local function open_project_kanban()
+  local cwd = vim.fn.getcwd()
+  local filename = ".kanban.md"
+  local filepath = cwd .. "/" .. filename
+
+  -- Create if missing
+  if vim.fn.filereadable(filepath) == 0 then
+    vim.cmd("KanbanCreate .kanban")
+  end
+
+  -- Always open explicitly
+  vim.cmd("KanbanOpen " .. filename)
+
+  -- Store window id (current window after open)
+  kanban_state.winid = vim.api.nvim_get_current_win()
+end
+
+local function close_project_kanban()
+  if kanban_state.winid and vim.api.nvim_win_is_valid(kanban_state.winid) then
+    -- Focus the kanban window
+    vim.api.nvim_set_current_win(kanban_state.winid)
+    vim.cmd("KanbanClose")
+    kanban_state.winid = nil
+  end
+end
+
+local function toggle_project_kanban()
+  if kanban_state.winid and vim.api.nvim_win_is_valid(kanban_state.winid) then
+    close_project_kanban()
+  else
+    open_project_kanban()
+  end
+end
+
+vim.keymap.set("n", "<leader>kb", toggle_project_kanban, { desc = "Toggle Project Kanban" })
--- vim/nvim/lua/plugins/navigation.lua
+++ vim/nvim/lua/plugins/navigation.lua
@@ -1,104 +1,113 @@
 return {
-	{
-		"stevearc/oil.nvim",
-		lazy = false,
-		dependencies = { { "nvim-mini/mini.icons", opts = {} } },
+  {
+    "stevearc/oil.nvim",
+    lazy = false,
+    dependencies = { { "nvim-mini/mini.icons", opts = {} } },
 
-		config = function()
-			-- Sensible split behavior
-			vim.opt.splitright = true
-			vim.opt.splitbelow = true
+    config = function()
+      -- Sensible split behavior
+      vim.opt.splitright = true
+      vim.opt.splitbelow = true
 
-			require("oil").setup({
-				default_file_explorer = true,
+      require("oil").setup {
+        default_file_explorer = true,
 
-				view_options = {
-					show_hidden = false,
-				},
+        view_options = {
+          show_hidden = false,
+        },
 
-				keymaps = {
-					-- Directory navigation
-					["<Tab>"] = "actions.select",
-					["<S-Tab>"] = "actions.parent",
-					["-"] = "actions.parent",
-					["q"] = "actions.close",
+        keymaps = {
+          -- Directory navigation
+          ["<Tab>"] = "actions.select",
+          ["<S-Tab>"] = "actions.parent",
+          ["-"] = "actions.parent",
+          ["q"] = "actions.close",
 
-					-- Open file
-					["<CR>"] = "actions.select",
+          -- Open file
+          ["<CR>"] = "actions.select",
 
-					-- Splits
-					["<C-v>"] = { "actions.select", opts = { vertical = true } },
-					["<C-x>"] = { "actions.select", opts = { horizontal = true } },
-					["<C-t>"] = { "actions.select", opts = { tab = true } },
-				},
-				float = {
-					-- Padding around the floating window
-					padding = 2,
-					-- max_width and max_height can be integers or a float between 0 and 1 (e.g. 0.4 for 40%)
-					max_width = 0.5,
-					max_height = 0.6,
-					border = nil,
-					win_options = {
-						winblend = 0,
-					},
-					-- optionally override the oil buffers window title with custom function: fun(winid: integer): string
-					get_win_title = nil,
-					-- preview_split: Split direction: "auto", "left", "right", "above", "below".
-					preview_split = "auto",
-					-- This is the config that will be passed to nvim_open_win.
-					-- Change values here to customize the layout
-					override = function(conf)
-						return conf
-					end,
-				},
-			})
+          -- Splits
+          ["<C-v>"] = { "actions.select", opts = { vertical = true } },
+          ["<C-x>"] = { "actions.select", opts = { horizontal = true } },
+          ["<C-t>"] = { "actions.select", opts = { tab = true } },
+        },
+        float = {
+          -- Padding around the floating window
+          padding = 2,
+          -- max_width and max_height can be integers or a float between 0 and 1 (e.g. 0.4 for 40%)
+          max_width = 0.5,
+          max_height = 0.6,
+          border = nil,
+          win_options = {
+            winblend = 0,
+          },
+          -- optionally override the oil buffers window title with custom function: fun(winid: integer): string
+          get_win_title = nil,
+          -- preview_split: Split direction: "auto", "left", "right", "above", "below".
+          preview_split = "auto",
+          -- This is the config that will be passed to nvim_open_win.
+          -- Change values here to customize the layout
+          override = function(conf)
+            return conf
+          end,
+        },
+      }
 
-			-- Floating Oil
-			vim.keymap.set("n", "<leader>of", function()
-				require("oil").toggle_float()
-			end, { desc = "Toggle Oil float" })
+      -- Floating Oil
+      vim.keymap.set("n", "<leader>of", function()
+        require("oil").toggle_float()
+      end, { desc = "Toggle Oil float" })
 
-			-- Toggle hidden files (works inside Oil buffer)
-			vim.keymap.set("n", "<leader>uh", function()
-				require("oil.actions").toggle_hidden.callback()
-			end, { desc = "Toggle hidden files in Oil" })
-		end,
-	},
-	{
-		"nvim-telescope/telescope.nvim",
-		dependencies = {
-			"nvim-lua/plenary.nvim",
-			{
-				"nvim-telescope/telescope-fzf-native.nvim",
-				build = "cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release --target install",
-			},
-		},
-		config = function()
-			local telescope = require("telescope")
-			local builtin = require("telescope.builtin")
-			local actions = require("telescope.actions")
+      -- Toggle hidden files (works inside Oil buffer)
+      vim.keymap.set("n", "<leader>uh", function()
+        require("oil.actions").toggle_hidden.callback()
+      end, { desc = "Toggle hidden files in Oil" })
+    end,
+  },
+  {
+    "nvim-telescope/telescope.nvim",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      {
+        "nvim-telescope/telescope-fzf-native.nvim",
+        build = "cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release --target install",
+      },
+    },
+    config = function()
+      local telescope = require "telescope"
+      local builtin = require "telescope.builtin"
+      local actions = require "telescope.actions"
 
-			telescope.setup({
-				defaults = {
-					mappings = {
-						i = {
-							["<Esc>"] = actions.close,
-						},
-					},
-				},
-			})
+      telescope.setup {
+        defaults = {
+          mappings = {
+            -- i = {
+            --   ["<Esc>"] = actions.close,
+            -- },
+            n = {
+              ["q"] = actions.close,
+            },
+          },
+        },
+      }
 
-			-- Core Pickers
-			vim.keymap.set("n", "<leader>ff", builtin.find_files, { desc = "Find files" })
-			vim.keymap.set("n", "<leader>fg", builtin.live_grep, { desc = "Live grep" })
-			vim.keymap.set("n", "<leader>fb", builtin.buffers, { desc = "Buffers" })
-			vim.keymap.set("n", "<leader>fr", builtin.oldfiles, { desc = "Recent files" })
+      -- Core Pickers
+      vim.keymap.set("n", "<leader>ff", builtin.find_files, { desc = "Find files" })
+      vim.keymap.set("n", "<leader>fg", builtin.live_grep, { desc = "Live grep" })
+      vim.keymap.set("n", "<leader>fb", builtin.buffers, { desc = "Buffers" })
+      vim.keymap.set("n", "<leader>fr", builtin.oldfiles, { desc = "Recent files" })
+      vim.keymap.set("n", "<leader>fh", function()
+        builtin.find_files {
+          hidden = true,
+          no_ignore = true,
+        }
+      end, { desc = "Find files (hidden + ignored)" })
 
-			-- LSP Pickers
-			vim.keymap.set("n", "gr", builtin.lsp_references, { desc = "LSP references" })
-			vim.keymap.set("n", "gd", builtin.lsp_definitions, { desc = "LSP definitions" })
-			vim.keymap.set("n", "gi", builtin.lsp_implementations, { desc = "LSP implementations" })
-			vim.keymap.set("n", "<leader>ds", builtin.diagnostics, { desc = "Diagnostics picker" })
-		end,
-	},
+      -- LSP Pickers
+      vim.keymap.set("n", "gr", builtin.lsp_references, { desc = "LSP references" })
+      vim.keymap.set("n", "gd", builtin.lsp_definitions, { desc = "LSP definitions" })
+      vim.keymap.set("n", "gi", builtin.lsp_implementations, { desc = "LSP implementations" })
+      vim.keymap.set("n", "<leader>ds", builtin.diagnostics, { desc = "Diagnostics picker" })
+    end,
+  },
 }
