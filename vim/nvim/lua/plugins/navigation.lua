54076959a5e2f16695d7b2e1b0f58ecb9de5ae48
Ed <edwardatkin@edwardatkin.co.uk>
2026-02-28 11:10:38 +0000

    feat(nvim): Add telescope keybinds search

--- vim/nvim/lua/plugins/navigation.lua
+++ vim/nvim/lua/plugins/navigation.lua
@@ -96,6 +96,7 @@ return {
       vim.keymap.set("n", "<leader>fg", builtin.live_grep, { desc = "Live grep" })
       vim.keymap.set("n", "<leader>fb", builtin.buffers, { desc = "Buffers" })
       vim.keymap.set("n", "<leader>fr", builtin.oldfiles, { desc = "Recent files" })
+      vim.keymap.set("n", "<leader>fk", builtin.keymaps, { desc = "Keymaps" })
       vim.keymap.set("n", "<leader>fh", function()
         builtin.find_files {
           hidden = true,
