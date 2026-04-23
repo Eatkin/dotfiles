7cf29f3595613d2581fdc7a0e01df52816402a51
Ed <edwardatkin@edwardatkin.co.uk>
2026-04-23 11:07:52 +0100

    patch(nvim): Add keybind for renaming tabs

--- vim/nvim/lua/plugins/buffers.lua
+++ vim/nvim/lua/plugins/buffers.lua
@@ -4,19 +4,26 @@ return {
     version = "*",
     dependencies = { "nvim-tree/nvim-web-devicons" },
     config = function()
-      require("bufferline").setup({
+      require("bufferline").setup {
         options = {
           numbers = "ordinal",
           show_buffer_close_icons = false,
           show_close_icon = false,
-        }
-      })
+        },
+      }
+
+      vim.keymap.set("n", "<leader>tr", function()
+        local name = vim.fn.input "Tab name: "
+        if name ~= "" then
+          vim.cmd("BufferLineTabRename " .. name)
+        end
+      end, { noremap = true, silent = true })
     end,
   },
   {
     "tiagovla/scope.nvim",
     config = function()
-      require("scope").setup({})
+      require("scope").setup {}
     end,
   },
 }
